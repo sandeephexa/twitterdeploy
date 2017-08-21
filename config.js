6fcc6ad59bdc617f51e01dc400fa268c02e503c6
38238-hexaware <31004655+38238-hexaware@users.noreply.github.com>
2017-08-21 09:35:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     consumer_key: 'ns4p3CGnrjo6llDDG7TjewkCL',
     consumer_secret: 'lQoJWbFX6CQdhOwMlDsG3dTVLtrxODBReBvjsf8oITue0Sfv1l',
-    access_token: '	899480335288684545-nru5VXGROETbXSMIzlD1XkLorPdCGfT',
+    access_token: '899480335288684545-nru5VXGROETbXSMIzlD1XkLorPdCGfT',
     access_token_secret: 'FslgrE6Y0wiWfgsZZpbgmIAtc8hycZ0AtZOq0Kk3pQwVy'
 }
